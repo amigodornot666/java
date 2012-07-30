0b0703cc4893aeac807c8e0d45d2b8b4d9026194
amigodornot666 <decimalwarrior@gmail.com>
2012-07-30 11:25:08 -0700

    added more

---
+++ MathClass1.java
@@ -0,0 +1,11 @@
+/*
+ * First math example in Java for Beginners
+ * MathClass1.java
+ */
+public class MathClass1 {
+				public static void main(String[] args) {
+								System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+				}
+}
+
+
