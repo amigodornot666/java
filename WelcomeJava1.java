3f688f1bbdfdc4cf82c7a37f82ec79db8848b495
amigodornot666 <decimalwarrior@gmail.com>
2012-07-30 10:53:34 -0700

    added a 2nd example

---
+++ WelcomeJava1.java
@@ -0,0 +1,11 @@
+/* This is the second simple java sample from "Java for Beginers"
+ * WelcomJava1.java
+ */
+public class WelcomeJava1 {
+				public static void main(String[] args) {
+								System.out.println("Programming is fun!!");
+								System.out.println("Fundamentals First.");
+								System.out.println("Problem driven!");
+				}
+}
+
