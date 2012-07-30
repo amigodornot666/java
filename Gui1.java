e462bdd710368a64cf1e93286fe35a50471e30b2
amigodornot666 <decimalwarrior@gmail.com>
2012-07-30 14:09:23 -0700

    added gui example demo from book.

---
+++ Gui1.java
@@ -0,0 +1,13 @@
+/*
+ * My first gui example from Java for Beginners
+ * Gui1.java
+ */ 
+import javax.swing.JOptionPane;
+
+public class Gui1 {
+				public static void main(String[] args) {
+								// Display Welcome to Java!!!
+								JOptionPane.showMessageDialog(null, "Welcome to Java!");
+				}
+}
+ 
